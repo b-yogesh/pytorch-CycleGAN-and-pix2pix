74e3eb258ba5d43fcee8785d68276e30c2f8f473
Sven Stauden <s9svstau@stud.uni-saarland.de>
2019-07-08 19:51:24 +0200

    added scripts folder for training

--- scripts/train_pix2pix_before2after.sh
+++
@@ -1,2 +0,0 @@
-set -ex
-python train.py --dataroot ./datasets/before2after_prepro_paired --name before2after_prepro --display_id 0 --model pix2pix --netG unet_256 --direction AtoB --lambda_L1 100 --dataset_mode aligned --norm batch --pool_size 0
